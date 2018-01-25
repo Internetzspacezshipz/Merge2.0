3a86b8fb44fc098732995109c0c53687b69eab97
GD106\ColeRempel <internetzspacezshipz@gmail.com>
2018-01-24 21:33:14 -0800

    Added audio to character and music

--- Assets/Scripts/Characters/PlayerControl.cs
+++ Assets/Scripts/Characters/PlayerControl.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -12,8 +13,18 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private CameraScript cameraScript;
 
+    //public AudioClip walk;
+    //public AudioClip jump;
+
+    private IEnumerator coroutine;
+
+
     private Rigidbody2D _RB;
     private SpriteRenderer _SR;
+    [SerializeField]
+    private AudioSource _ASWalking;
+    [SerializeField]
+    private AudioSource _ASJumping;
 
     [SerializeField]
     private Animator _Animator;
@@ -24,6 +35,9 @@ public class PlayerControl : MonoBehaviour
 
     private bool direction;
 
+    private bool audioPlaying = false;
+    private bool audioOn = true;
+
 
     #endregion
 
@@ -31,10 +45,20 @@ public class PlayerControl : MonoBehaviour
 
     private void Awake()
     {
+        //coroutine = audioCoroutine();
         _RB = GetComponent<Rigidbody2D>();
         _Animator = GetComponent<Animator>();
         _SR = GetComponent<SpriteRenderer>();
+        //_AS = GetComponent<AudioSource>();
     }
+
+
+    private void Start()
+    {
+        //StartCoroutine(coroutine);
+    }
+
+
 
     //add velocity
     internal void Move(float moveSpeed, int moveDirection)
@@ -42,10 +66,12 @@ public class PlayerControl : MonoBehaviour
         _RB.velocity = new Vector2(moveSpeed * moveDirection, _RB.velocity.y);
         if (_RB.velocity.x != 0)
         {
+
             _Animator.SetBool("IsWalking", true);
         }
         else
         {
+
             _Animator.SetBool("IsWalking", false);
         }
         if (moveDirection < 0)
@@ -57,18 +83,16 @@ public class PlayerControl : MonoBehaviour
         {
             direction = false;
             _SR.flipX = false;
-        }
-        else if (moveDirection == 0)
-        {
-            _SR.flipX = direction;
+
         }
     }
 
     //add jumping velocity
     protected void Jump(float jumpHeight)
     {
-
+        _ASJumping.Play();
         _RB.velocity = new Vector2(_RB.velocity.x, jumpHeight);
+
     }
 
 
@@ -78,11 +102,14 @@ public class PlayerControl : MonoBehaviour
         //movement inputs
         float movement = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Jump");
-      
+
         //if movement then move
         if (movement != 0)
         {
-
+            if (!_ASWalking.isPlaying && canJump == true)
+            {
+                _ASWalking.Play();
+            }
             if (movement < 0)
             {
                 Move(moveSpeed-windEffect, -1);
@@ -92,11 +119,14 @@ public class PlayerControl : MonoBehaviour
                 Move(moveSpeed+windEffect, 1);
             }
         }
-
         //Input.GetKeyDown
 
         else if (movement == 0)
         {
+            _ASWalking.Stop();
+
+            _SR.flipX = direction;
+
             Move(0+windEffect, 1);
         }
 
@@ -107,6 +137,12 @@ public class PlayerControl : MonoBehaviour
             {
                 Jump(jumpHeight);
             }
+        }
+
+        //animator here too pls
+        if (_RB.velocity.x == 0)
+        {
+
         }
 
         //animator here pls
@@ -154,8 +190,24 @@ public class PlayerControl : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        _ASWalking.Stop();
+
         canJump = false;
         gameObject.transform.parent = cameraScript.gameObject.transform;
 
     }
+
+
+    //private IEnumerator audioCoroutine()
+    //{
+    //    while (audioOn == true)
+    //    {
+    //        yield return new WaitForSeconds(0.1f);
+    //        while (audioPlaying == true)
+    //        {
+    //            _AS.PlayOneShot(walk);
+    //            yield return new WaitForSeconds(walk.length);
+    //        }
+    //    }
+    //}
 }
