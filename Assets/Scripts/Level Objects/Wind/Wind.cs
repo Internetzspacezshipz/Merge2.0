e750c482a80321bcca373cab719dc96c94b6d142
GD106\ColeRempel <internetzspacezshipz@gmail.com>
2018-01-24 22:31:25 -0800

    Wind: Added audio, improved usability.

--- Assets/Scripts/Level Objects/Wind/Wind.cs
+++ Assets/Scripts/Level Objects/Wind/Wind.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
-    [SerializeField]
     private PlayerControl player;
     [SerializeField]
     private float windStrength = 1f;
+
+    private AudioSource _AS;
+
+    private void Awake()
+    {
+        _AS = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            _AS.Play();
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
+
+            player = collision.GetComponentInParent<PlayerControl>();
             player.windEffect = windStrength;
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
+            _AS.Stop();
+
+            player = collision.GetComponentInParent<PlayerControl>();
             player.windEffect = 0;
         }
     }
