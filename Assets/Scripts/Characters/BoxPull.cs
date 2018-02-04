db67aad21c4f89f1627207eaa910dd457aeb82c3
GD106\ColeRempel <internetzspacezshipz@gmail.com>
2018-02-03 18:45:48 -0800

    FIXED BOX FINALLY

--- Assets/Scripts/Characters/BoxPull.cs
+++ Assets/Scripts/Characters/BoxPull.cs
@@ -4,18 +4,63 @@ using UnityEngine;
 
 public class BoxPull : MonoBehaviour
 {
-    CharacterController controller;
+    PlayerControl controller;
+    GameObject heldBox = null;
+    bool selectTime = false;
+    [SerializeField]
+    private Vector3 holdLocation = new Vector3(1.3f, -0.67f, 0);
 
+    //            Debug.Log("This Ran");
 
     private void Awake()
     {
-        controller = GetComponentInParent<CharacterController>();
+        controller = GetComponentInParent<PlayerControl>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Box") && Input.GetKeyDown(KeyCode.E) && heldBox == null && selectTime == false && controller.boxHeld == false)
+        {
+            other.transform.SetParent(controller.gameObject.transform, false);
+            Destroy(other.GetComponent<Rigidbody2D>());
+            other.transform.position = controller.transform.position + holdLocation;
+            Debug.Log("This Ran");
+            heldBox = other.gameObject;
+            StartCoroutine(WaitForUnselect());
+            controller.boxHeld = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (heldBox != null && selectTime == false)
+        {
+            if (controller.canJump == false)
+            {
+                Debug.Log("This Ran Set Null Parent");
+                heldBox.gameObject.AddComponent<Rigidbody2D>();
+
+                heldBox.transform.parent = null;
+                heldBox = null;
+                controller.boxHeld = false;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("This Ran Set Null Parent");
+                heldBox.gameObject.AddComponent<Rigidbody2D>();
+                heldBox.transform.parent = null;
+                heldBox = null;
+                controller.boxHeld = false;
+            }
+        }
     }
 
 
-
-
-
-
-
+    private IEnumerator WaitForUnselect()
+    {
+        selectTime = true;
+        yield return new WaitForSeconds(0.6f);
+        selectTime = false;
+    }
 }
