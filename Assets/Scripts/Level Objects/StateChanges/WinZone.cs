0bb35242c113beb8adf31916578ae45fd9cbde1a
VFS/gd48pedro <pedrocamachoengenharia@gmail.com>
2018-02-05 13:41:19 -0800

    Add Gizmo to WinZone

--- Assets/Scripts/Level Objects/StateChanges/WinZone.cs
+++ Assets/Scripts/Level Objects/StateChanges/WinZone.cs
@@ -6,6 +6,11 @@ public class WinZone : MonoBehaviour
 {
     [SerializeField]
     private string levelToLoad = "MainMenu";
+    
+    private Vector2 GizmoSize;
+     
+
+   
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -13,5 +18,12 @@ public class WinZone : MonoBehaviour
         {
             GameManager.instance.LoadLevel(levelToLoad);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        GizmoSize = GetComponent<BoxCollider2D>().size;
+        Gizmos.color = new Color(0,1f,0,0.2f);
+        Gizmos.DrawCube(transform.position, new Vector3(GizmoSize.x, GizmoSize.y));
     }
 }
